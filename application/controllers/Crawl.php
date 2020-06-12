59efc85b48fee761eb0ec5d2f4a068999539d414
root <root@denguru.kr>
2020-06-12 09:39:18 +0900

    b

--- application/controllers/Crawl.php
+++ application/controllers/Crawl.php
@@ -3693,7 +3693,7 @@ $img_src_array = parse_url(urldecode($imageUrl));
                 exit(json_encode($result,JSON_UNESCAPED_UNICODE));
             }
 
-            if (empty($this->input->post('crw_price_sale'))) {
+            if ($this->input->post('crw_price_sale') == '') {
                 $result = array('resultcode'=>1009,'message' => 'crw_price_sale 가 없습니다.');
                 exit(json_encode($result,JSON_UNESCAPED_UNICODE));
             }
