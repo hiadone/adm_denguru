98fc2408b4511a50af771f22ac58c850a5b67e43
root <root@denguru.kr>
2020-07-10 14:52:56 +0900

    b

--- views/admin/basic/cmall/crawlitem/index.php
+++ views/admin/basic/cmall/crawlitem/index.php
@@ -75,7 +75,7 @@
 								
 								<?php if (element('crw_file_1', $result)) {?>
 									
-										<img src="<?php echo thumb_url('crawlitem', element('crw_file_1', $result), 80); ?>" alt="<?php echo html_escape(element('crw_name', $result)); ?>" title="<?php echo html_escape(element('crw_name', $result)); ?>" class="thumbnail mg0" style="width:80px;" 
+										<img src="<?php echo thumb_url('crawlitem', element('crw_file_1', $result), 80); ?>" alt="<?php echo html_escape(element('crw_name', $result)); ?>" title="<?php echo html_escape(element('crw_name', $result)); ?>" class="thumbnail mg0 view_full_image" style="width:80px;" data-origin-image-url="<?php echo thumb_url('crawlitem', element('crw_file_1', $result)); ?>"
 
 
 										 />
@@ -86,8 +86,8 @@
 								
 								<?php if (element('cdt_file_1', $result)) {?>
 									
-										<img src="<?php echo thumb_url('crawlitemdetail', element('cdt_file_1', $result), 80); ?>" alt="<?php echo html_escape(element('crw_name', $result)); ?>" title="<?php echo html_escape(element('crw_name', $result)); ?>" class="thumbnail mg0" style="width:80px;height:100px;" 
-class="view_full_image"  data-origin-image-url="<?php echo thumb_url('crawlitemdetail', element('cdt_file_1', $result)); ?>"
+										<img src="<?php echo thumb_url('crawlitemdetail', element('cdt_file_1', $result), 80); ?>" alt="<?php echo html_escape(element('crw_name', $result)); ?>" title="<?php echo html_escape(element('crw_name', $result)); ?>" class="thumbnail mg0 view_full_image" style="width:80px;height:100px;" 
+ 
 										/>
 									
 								<?php } ?>
