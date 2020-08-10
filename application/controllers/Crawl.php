5c9176d1e6205408ace0aab6de8c0e2cfb4f0df6
root <root@denguru.kr>
2020-08-10 12:44:01 +0900

    b

--- application/controllers/Crawl.php
+++ application/controllers/Crawl.php
@@ -3025,7 +3025,8 @@ class Crawl extends CB_Controller
             }
         }
         
-        
+//         print_r2($data['list']);
+// exit;
         // foreach ($storelist as $skey => $sval) {
 
         //     $flag=true;
@@ -3150,7 +3151,7 @@ class Crawl extends CB_Controller
             preg_match_all($pattern, $str, $match);
             $crw_category1 = implode('', $match[0]);
             
-            $crw_category1 = preg_replace("/[ #\&\-%=\/\\\:;\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i", "", $crw_category1);
+            $crw_category1 = preg_replace("/[ #\&\+\-%@=\/\\\:;,\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i", "", $crw_category1);
 
 
 
@@ -3263,7 +3264,7 @@ class Crawl extends CB_Controller
                     // preg_match_all($pattern, $str, $match);
 
                     $updatedata['crw_category' . $k] = json_decode(sprintf('"%s"', $str));
-                    $updatedata['crw_category' . $k] = preg_replace("/[ #\&\-%=\/\\\:;\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i", "", $updatedata['crw_category' . $k]);
+                    $updatedata['crw_category' . $k] = preg_replace("/[ #\&\+\-%@=\/\\\:;,\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i", "", $updatedata['crw_category' . $k]);
                     // $updatedata['crw_category' . $k] = implode('', $match[0]);
                     
                 }
@@ -3337,7 +3338,7 @@ class Crawl extends CB_Controller
                         // preg_match_all($pattern, $str, $match);
                         $updatedata['crw_category' . $k] = json_decode(sprintf('"%s"', $str));
 
-                        $updatedata['crw_category' . $k] = preg_replace("/[ #\&\-%=\/\\\:;\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i", "", $updatedata['crw_category' . $k]);
+                        $updatedata['crw_category' . $k] = preg_replace("/[ #\&\+\-%@=\/\\\:;,\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i", "", $updatedata['crw_category' . $k]);
                     // }
                 }
            
