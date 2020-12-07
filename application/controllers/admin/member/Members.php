b2dec22ca33894dde4844a632fc75d75c84faa7e
root <root@denguru.kr>
2020-12-07 12:38:42 +0900

    b

--- application/controllers/admin/member/Members.php
+++ application/controllers/admin/member/Members.php
@@ -335,7 +335,7 @@ class Members extends CB_Controller
 			$config[] = array(
 				'field' => 'mem_userid',
 				'label' => '회원아이디',
-				'rules' => 'trim|required|min_length[3]|max_length[40]|is_unique[member_userid.mem_userid.mem_id.' . element('mem_id', $getdata) . ']|callback__mem_userid_check',
+				'rules' => 'trim|required|min_length[3]|max_length[50]|is_unique[member_userid.mem_userid.mem_id.' . element('mem_id', $getdata) . ']|callback__mem_userid_check',
 			);
 			$config[] = array(
 				'field' => 'mem_password',
@@ -356,7 +356,7 @@ class Members extends CB_Controller
 			$config[] = array(
 				'field' => 'mem_userid',
 				'label' => '회원아이디',
-				'rules' => 'trim|required|alphanumunder|min_length[3]|max_length[50]|is_unique[member_userid.mem_userid]',
+				'rules' => 'trim|required|min_length[3]|max_length[50]|is_unique[member_userid.mem_userid]',
 			);
 			$config[] = array(
 				'field' => 'mem_password',
